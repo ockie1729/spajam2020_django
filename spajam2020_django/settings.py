a4b696774fbe9b8c023b595e5a9e4793cd3dda07
mofumofuchan <you_oki1@yahoo.co.jp>
2020-09-27 21:52:17 +0900

    settings.pyにApiServerConfigを追加

--- spajam2020_django/settings.py
+++ spajam2020_django/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'api_server.apps.ApiServerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
