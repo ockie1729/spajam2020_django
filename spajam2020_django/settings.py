1b1087d708c6d285d0035529cc45ded366a7947a
mofumofuchan <you_oki1@yahoo.co.jp>
2020-09-28 01:19:31 +0900

    Revert "Watsonアプリを追加"
    This reverts commit 10be46dc3b8584d507b5a566a96999fb3346bafc.

--- spajam2020_django/settings.py
+++ spajam2020_django/settings.py
@@ -30,7 +30,6 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'api_server.apps.ApiServerConfig',
-    'watson.apps.WatsonConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- watson/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WatsonConfig(AppConfig):
-    name = 'watson'
