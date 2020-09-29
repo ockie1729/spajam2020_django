40466c1f9a2da6defc2b8cca7942c667bbfb9ba5
Lily <lim.intefx@gmail.com>
2020-09-29 22:54:42 +0900

    GirlsLoveモデルのindex apiを実装

--- sample_api/views.py
+++ sample_api/views.py
@@ -5,4 +5,14 @@ from .models import GirlsLove
 def index(request):
     girls = GirlsLove.objects.all()
 
-    return JsonResponse(data=girls)
+    content = []
+    for girl in girls:
+        girl_dict = {}
+        girl_dict["follower_name"] = girl.follower_name
+        girl_dict["followee_name"] = girl.followee_name
+        content.append(girl_dict)
+
+    response = {}
+    response["message"] = "OK"
+    response["content"] = content
+    return JsonResponse(data=response)
