cf14e75a5e989c87b1145ceecbd588c32ca15781
d-yuji <d.yuji.fm@gmail.com>
2020-10-02 22:08:14 +0900

    INFOログの設定と出力

--- api_server/views.py
+++ api_server/views.py
@@ -1,7 +1,11 @@
 from django.http import JsonResponse
 
+import logging
+
 
 def index(request):
+    logger = logging.getLogger(__name__)
+    logger.info("hello world")
     return JsonResponse(
         data={'message': 'Hello, world from API server index, deployed with GitHub actions triggered by PR merge'}
     )
