fb8d58629d3b8c44aab49ac9c3d79d267e28a856
mofumofuchan <you_oki1@yahoo.co.jp>
2020-10-03 09:17:14 +0900

    GirlsLoveを追加するエンドポイントを作成

--- sample_api/views.py
+++ sample_api/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import GirlsLove
+import json
 
 
 @csrf_exempt
@@ -20,6 +21,14 @@ def index(request):
         response["content"] = content
         return JsonResponse(data=response)
     elif request.method == 'POST':
-        return JsonResponse(data={"message": "nothing done"})
+        request_json = json.loads(request.body)
+
+        girls_love = GirlsLove()
+        girls_love.follower_name = request_json['follower_name']
+        girls_love.followee_name = request_json['followee_name']
+        girls_love.save()
+
+        return JsonResponse(data={"message":
+                                  "successfully added a new girls love"})
     else:
         return JsonResponse(data={"message": "unexpetected http method"}, status=400)
