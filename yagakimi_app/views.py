e224924a9edb11437240ff39b6dbae9ab8dec9ef
Lily <lim.intefx@gmail.com>
2020-10-04 00:22:10 +0900

    以下の作業でユーザーの追加をできるようにした - yagakimi_app/urls.pyを作成しuser_createへのエンドポイントを追加 - spajam2020django/urls.pyにyagakimi_appを追加する - viewsでデータ登録の処理であるuser_createを実装(現状はnameをuuid以外は空文字)

--- yagakimi_app/views.py
+++ yagakimi_app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http.response import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from .models import User
 
 # Create your views here.
+
+
+@csrf_exempt
+def user_create(request):
+
+    if request.method == 'POST':
+        request_json = json.loads(request.body)
+        user = User()
+        user.uuid = request_json["uuid"]
+        user.name = ""
+        user.twitter_id = ""
+        user.password = ""
+        user.save()
+        return JsonResponse(data={"message": "successfully added a new user"})
