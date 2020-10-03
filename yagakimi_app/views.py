c6c5005f3b105b74b4e31a86af5ef5fa5b175662
mofumofuchan <you_oki1@yahoo.co.jp>
2020-10-04 01:41:50 +0900

    テキストを受け取り，Watsonに問い合わせるAPIを実装

--- yagakimi_app/views.py
+++ yagakimi_app/views.py
@@ -1,9 +1,36 @@
+import json
+import os
+import copy
+import datetime
+
+import requests
+from requests.auth import HTTPBasicAuth
+
 from django.http.response import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-import json
-from .models import User
 
-# Create your views here.
+from .models import User, Text
+
+WATSON_CONFIG = {
+    'user': 'apikey',
+    'pass': os.environ['WATSON_PASSWORD'],
+    'headers': {'Content-type': 'application/json'},
+    'url': 'https://api.jp-tok.natural-language-understanding.watson.cloud.ibm.com/instances/8674a94a-20ad-42ed-b0ce-3321526d6231/v1/analyze',
+    'params': {'version': '2018-11-16'},
+    'data': {
+        "text": "",
+        "features": {
+            "categories": {},
+            "keywords": {
+                "emotion": True
+            },
+            "entities": {},
+            "concepts": {
+                "limit": 3
+            },
+        },
+    }
+}
 
 
 @csrf_exempt
@@ -20,3 +47,48 @@ def user_create(request):
         return JsonResponse(data={"message": "successfully added a new user"})
     else:
         return JsonResponse(data={"message": "only POST is acceptalbe"}, status=400)
+
+
+@csrf_exempt
+def topic_register_text(request):
+
+    if request.method == 'POST':
+        api_call_time = datetime.datetime.now()  # モデルのcreated_atに使用
+
+        request_json = json.loads(request.body)
+        request_text = request_json['text']
+        request_webrtc_room_id = request_json['webrtc_room_id']
+        request_uuid = request_json['uuid']
+        request_is_relax_str = request_json['is_relax']  # TODO 現在はまだ使用せず
+
+        # Watson APIを呼び出し
+        request_watson_config = copy.deepcopy(WATSON_CONFIG)
+        request_watson_config['data']["text"] = request_text
+
+        res = requests.post(request_watson_config['url'],
+                            params=request_watson_config['params'],
+                            data=json.dumps(request_watson_config['data']),
+                            headers=request_watson_config['headers'],
+                            auth=HTTPBasicAuth(request_watson_config['user'],
+                                               request_watson_config['pass']))
+        watson_results = json.loads(res.text)
+
+        # TODO 形態素解析を実行
+
+        # Textモデルを作成し，値を保存
+        new_text = Text()
+        new_text.text = request_text
+        new_text.watson_response = json.dumps(watson_results)
+        new_text.created_at = api_call_time
+        new_text.uuid = request_uuid
+        # new_text.webrtc_room_id = request_webrtc_room_id  # TODO カラムが追加されたら実装
+        # new_text.tokenized_text  # TODO 未実装
+        new_text.save()
+
+        return JsonResponse(
+            data={"message": "suggessfully registered a text message",
+                  "watson_response": watson_results}
+        )
+    else:
+        return JsonResponse(data={"message": "only POST is acceptalbe"},
+                            status=400)
