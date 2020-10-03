506cd71dd4b7e7f4e37a7d179fa70edc599d084a
Lily <lim.intefx@gmail.com>
2020-10-04 01:38:53 +0900

    makemigrations を実行。 また、default値を '' に設定

--- yagakimi_app/models.py
+++ yagakimi_app/models.py
@@ -21,4 +21,4 @@ class Text(models.Model):
     watson_response = models.TextField()
     tokenized_text = models.TextField()
     created_at = models.DateTimeField()
-    webrtc_room_id = models.CharField(max_length=50)
+    webrtc_room_id = models.CharField(max_length=50, default='')
