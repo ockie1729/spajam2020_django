e7d741d235305e87e73f36057a03792b888026d9
Lily <lim.intefx@gmail.com>
2020-10-03 22:57:44 +0900

    各種モデルを追加

--- yagakimi_app/models.py
+++ yagakimi_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    uuid = models.CharField(max_length=50)
+    twitter_id = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+
+
+class Room(models.Model):
+    webrtc_room_id = models.CharField(max_length=50)
+    uuid = models.CharField(max_length=50)
+
+
+class Text(models.Model):
+    text = models.TextField()
+    uuid = models.CharField(max_length=50)
+    watson_response = models.TextField()
+    tokenized_text = models.TextField()
+    created_at = models.DateTimeField()
