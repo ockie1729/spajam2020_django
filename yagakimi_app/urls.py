b701cd598776b8dda20d306dff6175fb1b6a28d0
Lily <lim.intefx@gmail.com>
2020-10-04 02:26:11 +0900

    - LINEにルームIDを投稿する処理を追加 - それに伴いrequirements.txtにライブラリを追加 - IDの投稿先は現状は決め打ち - roomモデルは保存しなくて良さそうなので保存していない

--- yagakimi_app/urls.py
+++ yagakimi_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('user/create', views.user_create, name='user_create'),
+    path('line/push', views.push_room_id_to_line, name='push_room_id_to_line')
 ]
--- yagakimi_app/views.py
+++ yagakimi_app/views.py
@@ -1,9 +1,14 @@
 from django.http.response import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
-from .models import User
+from .models import User, Room
+import os
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
 
 # Create your views here.
+
+line_bot_api = LineBotApi(os.environ['LINE_TOKEN'])
 
 
 @csrf_exempt
@@ -18,5 +23,18 @@ def user_create(request):
         user.password = ""
         user.save()
         return JsonResponse(data={"message": "successfully added a new user"})
+    else:
+        return JsonResponse(data={"message": "only POST is acceptalbe"}, status=400)
+
+
+@csrf_exempt
+def push_room_id_to_line(request):
+
+    if request.method == 'POST':
+        request_json = json.loads(request.body)
+        line_messsage = request_json["line_messsage"]
+        line_bot_api.push_message(
+            os.environ['ROOM_ID'], TextSendMessage(text=line_messsage))
+        return JsonResponse(data={"message": "successfully pushed line message"})
     else:
         return JsonResponse(data={"message": "only POST is acceptalbe"}, status=400)
